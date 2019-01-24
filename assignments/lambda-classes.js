772a12790b508f36b39c9e28cba0d419236e6bce
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-01-24 16:24:26 -0500

    added tests & finished classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -66,3 +66,154 @@ class ProjectManager extends Instructor{
         console.log(`${this.name} debugs ${student.name}\'s code on ${subject}`);
     }
 };
+
+
+/* ===== TESTS ===== */
+
+/* PERSON CLASS */
+
+const andrew = new Person({
+    name: 'Andrew',
+    age: 26,
+    location: 'Seattle, WA',
+    gender: 'male'
+});
+
+const john = new Person({
+    name: 'John',
+    age: 58,
+    location: 'San Francisco, CA',
+    gender: 'male'
+});
+
+const christina = new Person({
+    name: 'Christina',
+    age: 24,
+    location: 'New York City, NY',
+    gender: 'female'
+});
+
+console.log(andrew);
+console.log(john);
+console.log(christina);
+christina.speak();
+andrew.speak();
+john.speak();
+
+/* STUDENT CLASS */
+
+const david = new Student({
+    name: 'David',
+    location: 'Winnipeg, MB',
+    age: 23,
+    gender: 'male',
+    previousBackground: 'waiter',
+    className: 'CS01',
+    favSubjects: ['HTML5', 'CSS3', 'Algorithms and Data Structures'] 
+});
+
+const susan = new Student({
+    name: 'Susan',
+    location: 'San Diego, CA',
+    age: 26,
+    gender: 'female',
+    previousBackground: 'business development manager',
+    className: 'CS06',
+    favSubjects: ['Algorithms and Data Structures', 'Python'] 
+});
+
+const karen = new Student({
+    name: 'Karen',
+    location: 'Phoenix, AZ',
+    age: 29,
+    gender: 'female',
+    previousBackground: 'marketing manager',
+    className: 'DS705',
+    favSubjects: ['Descriptive statistics', 'Python', 'Reinforcement Learning', 'Natural Language Processing'] 
+});
+
+console.log(david);
+console.log(susan);
+console.log(karen);
+david.listsSubjects();
+susan.listsSubjects();
+karen.PRAssignment('Python');
+susan.PRAssignment('HTML5');
+david.sprintChallenge('Advanced CSS');
+karen.sprintChallenge('Natural Language Processing');
+
+/* INSTRUCTOR CLASS */
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: 'Don\'t forget the homies'
+});
+
+const brad = new Instructor({
+    name: 'Brad',
+    location: 'Vancouver, BC',
+    age: 44,
+    gender: 'male',
+    favLanguage: 'Python',
+    specialty: 'Back-end',
+    catchPhrase: 'Meet me on the other side'
+});
+
+const ryan = new Instructor({
+    name: 'Ryan',
+    location: 'Provo, UT',
+    age: 33,
+    gender: 'male',
+    favLanguage: 'Python',
+    specialty: 'Data Science',
+    catchPhrase: 'Yoooo I am coming!'
+});
+
+console.log(fred);
+console.log(brad);
+console.log(ryan);
+fred.demo('JavaScript');
+brad.demo('Django');
+brad.demo('Authentication');
+ryan.demo('Unsupervised Learning');
+ryan.grade(david);
+brad.grade(karen);
+fred.grade(susan);
+
+/* PROJECT MANAGER CLASS */
+
+const lisa = new ProjectManager({
+    name: 'Lisa',
+    location: 'Miami, FL',
+    age: 36,
+    gender: 'female',
+    favLanguage: 'Rust',
+    catchPhrase: 'Here I am!',
+    specialty: 'Data Science',
+    gradClassName: 'DS59',
+    favInstructor: ryan
+});
+
+const james = new ProjectManager({
+    name: 'James',
+    location: 'Denver, CO',
+    age: 29,
+    gender: 'male',
+    favLanguage: 'C++',
+    catchPhrase: 'I am the only one!',
+    specialty: 'Full Stack Web',
+    gradClassName: 'CS03',
+    favInstructor: fred
+});
+
+console.log(lisa);
+console.log(james);
+lisa.standUp('WEB17');
+james.standUp('CS999');
+lisa.debugsCode(susan, 'Advanced README.md');
+james.debugsCode(karen, 'Natural Language Understanding');
